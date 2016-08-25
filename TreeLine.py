0c3e742b4aab0604010e3461ab19491074a4143b
jdegene <j-f-d@gmx.de>
2016-08-25 16:07:25 +0200

    Bugfix

--- TreeLine.py
+++ TreeLine.py
@@ -15,15 +15,19 @@ from arcpy.sa import *
 # Set Environments
 arcpy.env.overwriteOutput = True
 arcpy.CheckOutExtension("Spatial")
+arcpy.env.workspace = 'D:/Test/Michael/Wald_Jan/Scratch/'
 
 # Inputs / Outputs
 inDGM = 'D:/Test/Michael/Wald_Jan/DGM_Mongolei.tif'
-inForest = 'D:/Test/Michael/Wald_Jan/Wald_Raster.tif'
-outFile = 'D:/Test/Michael/Wald_Jan/Waldgrenze.tif'
+inForest = 'D:/Test/Michael/Wald_Jan/Wald_Raster_90m.tif'
+outFile = 'D:/Test/Michael/Wald_Jan/Waldgrenzen/Waldgrenze.tif'
 
 # Vicinity to check
-forestRadius = 3 # is there forest at higher altitude in this radius
-nonForest = 4    # is there a higher elevation at all in this radius
+forestRadius = 5 # is there forest at higher altitude in this radius
+nonForest = 5    # is there a higher elevation at all in this radius
+
+# Altitude difference in meter, mini
+alt_diff = 50
 
 
 ##################################
@@ -39,18 +43,19 @@ arcpy.MakeRasterLayer_management(inDGM, "dgm_layer")
 arcpy.MakeRasterLayer_management(alt_forst, "alt_forest_layer")
 
 # create Focal Statistics, each pixel in the output raster gets the maximum value of the cell in radius
-alt_forstFoc = FocalStatistics("alt_forest_layer", NbrAnnulus(1, forestRadius, "CELL"), 
+alt_forstFoc = FocalStatistics("alt_forest_layer", NbrAnnulus(0, forestRadius, "CELL"), 
                                "MAXIMUM", "DATA")
-dgmFoc = FocalStatistics("dgm_layer", NbrAnnulus(1, nonForest, "CELL"), 
+dgmFoc = FocalStatistics("dgm_layer", NbrAnnulus(0, nonForest, "CELL"), 
                                "MAXIMUM", "DATA")
 
 # create new raster with 1 where highest altitude in forest in vicinity and where higher elev. exisits
-outCon = Con( (alt_forst == alt_forstFoc) & (dgmFoc > dgm), 1)
-outCon.save(outFile)
+outCon = Con( (alt_forst == alt_forstFoc) & (dgmFoc-alt_diff > dgm), 1)
+#outCon = Con( (alt_forst == alt_forstFoc), 1)
+outCon.save(outFile[:-4] + "_" + str(forestRadius) + "_" + str(nonForest) + "_" + str(alt_diff) + outFile[-4:])
 
 # Delete files created on HDD
-arcpy.Delete_management(alt_forstFoc)
-arcpy.Delete_management(dgmFoc )
+#arcpy.Delete_management(alt_forstFoc)
+#arcpy.Delete_management(dgmFoc )
 
 
 arcpy.CheckInExtension("Spatial")
