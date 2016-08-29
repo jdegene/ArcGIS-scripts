27bf75322a06e25a28261c6ac996c71a298c876f
jdegene <j-f-d@gmx.de>
2016-08-29 10:49:18 +0200

    lower tree line added, slope exclusion added, cleaned

--- TreeLine.py
+++ TreeLine.py
@@ -11,71 +11,94 @@ from arcpy.sa import *
 
 
 ##################################
+ # ENVIRONMENTS AND PARAMETERS #
+##################################
 
 
 # Set Environments
 arcpy.env.overwriteOutput = True
-arcpy.CheckOutExtension("Spatial")
+#arcpy.env.cellSize = 'MINOF'
+#arcpy.env.snapRaster = 'D:/Test/Michael/Wald_Jan/Wald_Raster_90m.tif'
 arcpy.env.workspace = 'D:/Test/Michael/Wald_Jan/Scratch/'
 
+arcpy.CheckOutExtension("Spatial")
+
+
+
 # Inputs / Outputs
-inDGM = 'D:/Test/Michael/Wald_Jan/DGM_Mongolei_SRTM_90.tif'
-inForest = 'D:/Test/Michael/Wald_Jan/Wald_Raster_90m.tif'
-outFile = 'D:/Test/Michael/Wald_Jan/Waldgrenzen/Waldgrenze.tif'
+inDGM = 'D:/Test/Michael/Wald_Jan/DGM_Mongolei_SRTM_90.tif' # DEM file in meters altitude
+inForest = 'D:/Test/Michael/Wald_Jan/Wald_Raster_90m.tif'   # Forest file, 0 = Forest, NoData = no forest
+inSlope = 'D:/Test/Michael/Wald_Jan/Slope_WGS_degree.tif'       # Slope in Degree per Pixel
+
+outFile = 'D:/Test/Michael/Wald_Jan/Waldgrenzen/Waldgrenze.tif' # Basic output name, will be extended by parameters
+
 
 # Vicinity to check
 forestRadius = 5 # is there forest at higher altitude in this radius
 nonForest = 5    # is there a higher elevation at all in this radius
 
-# Check vicinity for altitude differences, if diff is below threshold, terrain is assumed flat and point excluded
-flatRadius = 5  #cells
-flatDiff = 100    #meters
-
-# Altitude difference in meter, mini
+# Altitude difference in meter, minimum slope
 alt_diff = 50
+minSlope = 2
 
 
 ##################################
+ # MAIN CALCULATION PART #
+##################################
+
 
 dgm = Raster(inDGM)
+slope = Raster(inSlope)
 forest = Raster(inForest)
 forest1 = forest+1 # change forest raster values from 0 to 1
+
 
 # Get altitude values only where forest exisits
 alt_forst = SetNull(forest, dgm, "VALUE <> 0")
 
+
 # Create Raster Layers from input data
 arcpy.MakeRasterLayer_management(inDGM, "dgm_layer")
 arcpy.MakeRasterLayer_management(alt_forst, "alt_forest_layer")
-arcpy.MakeRasterLayer_management(forest1, "forest1_layer")
 
-# create Focal Statistics, each pixel in the output raster gets the maximum value of the cell in radius
-alt_forstFoc = FocalStatistics("alt_forest_layer", NbrAnnulus(0, forestRadius, "CELL"), 
-                               "MAXIMUM", "DATA")
-dgmFoc = FocalStatistics("dgm_layer", NbrAnnulus(0, nonForest, "CELL"), 
-                               "MAXIMUM", "DATA")
 
-# Focal Stats to decrease points
-altDiffFoc = FocalStatistics("dgm_layer", NbrAnnulus(0, nonForest, "CELL"), 
-                               "RANGE", "DATA")
-minForestFoc = FocalStatistics("forest1_layer", NbrAnnulus(0, 10, "CELL"), 
-                               "SUM", "DATA")
+# create Focal Statistics, each pixel in the output raster gets the max/min value of the cell in radius
+alt_max_forstFoc = FocalStatistics("alt_forest_layer", NbrAnnulus(0, forestRadius, "CELL"), 
+                               "MAXIMUM", "DATA")
+alt_min_forstFoc = FocalStatistics("alt_forest_layer", NbrAnnulus(0, forestRadius, "CELL"), 
+                               "MINIMUM", "DATA")
+dgmFoc_max = FocalStatistics("dgm_layer", NbrAnnulus(0, nonForest, "CELL"), 
+                               "MAXIMUM", "DATA")
+dgmFoc_min = FocalStatistics("dgm_layer", NbrAnnulus(0, nonForest, "CELL"), 
+                               "MINIMUM", "DATA")
+
 
 # create new raster with 1 where highest altitude in forest in vicinity and where higher elev. exisits
-outCon = Con( (alt_forst == alt_forstFoc) &
-              (dgmFoc-alt_diff > dgm) &
-              (altDiffFoc > 50) &
-              (minForestFoc > 10), 1)
+outCon = Con( (alt_forst == alt_max_forstFoc) &
+              (dgmFoc_max - alt_diff > dgm) &
+              (slope > minSlope), 1, # upper tree boundary 
+
+              Con( (alt_forst == alt_max_forstFoc) &
+                   (dgmFoc_min+alt_diff > dgm) &
+                   (slope > minSlope), 2) # lower tree boundary
+              
+              )
+
+
+##################################
+ # OUTPUT CREATION AND DELETION #
+##################################
 
 outCon.save(outFile[:-4] + "_" + str(forestRadius) + "_" +
-            str(nonForest) + "_" + str(alt_diff) + "_" +
-            str(flatRadius) + "_" + str(flatDiff) + outFile[-4:])
+            str(nonForest) + "_" +
+            str(minSlope) + outFile[-4:])
 
 
 # Delete files created on HDD
-arcpy.Delete_management(alt_forstFoc)
-arcpy.Delete_management(dgmFoc)
-arcpy.Delete_management(altDiffFoc)
+arcpy.Delete_management(alt_max_forstFoc)
+arcpy.Delete_management(alt_min_forstFoc)
+arcpy.Delete_management(dgmFoc_max)
+arcpy.Delete_management(dgmFoc_min)
 
 
 arcpy.CheckInExtension("Spatial")
